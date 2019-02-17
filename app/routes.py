7aaf77c2cabed93515dbe7a8b603d336adda8504
Joshua Miller <joshua.miller@rd.io>
2019-02-17 15:43:00 -0600

    template extends

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,15 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-	user = {'username': 'joshua'}
-	return render_template('index.html', title='Home', user=user)
+	user = {'username': 'Joshua'}
+	entries = [
+	    {
+	        'author': {'username': 'John'},
+	        'content': 'Today is a wonderful today'
+	    },
+	    {
+	        'author': {'username': 'John'},
+	        'content': 'Yesterday was good also'	        
+	    }
+	]
+	return render_template('index.html', title='Home', user=user, entries=entries)
