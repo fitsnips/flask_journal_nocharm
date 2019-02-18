0fafeb5d09f37661be3764b3c5312e557e457e39
Joshua Miller <joshua.miller@rd.io>
2019-02-17 21:08:26 -0600

    inital migrations

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'YjZm7YTM3zOWRkODJlNz1BjYWUyY3jY5M4jBm'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
+	    'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
