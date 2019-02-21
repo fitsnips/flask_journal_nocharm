b2dcde7c100951ec3559a4e5d6b05c839fd6a1f2
Joshua Miller <joshua.miller@rd.io>
2019-02-20 22:23:55 -0600

    starting login code

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+form app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -25,8 +27,19 @@ def about():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	form = LoginForm()
-	if form.validate_on_submit():
-		flash('Login requested for user {}, remember_me={}'.format(
-			form.username.data, form.remember_me.data))
+	if current_user.is_authenticated:
 		return redirect(url_for('index'))
-	return render_template('login.html', title='Sign In', form=form)
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		if user is None or not user.check_password(form.password.data):
+			flash('Invalid username or password')
+			return redirect(url('login'))
+		login_user(user, remember=form.remember_me.data)
+		return redirect(url_for('index'))
+	return render_template('login.html', title='Sign in', form=form)
+
+@app.route('logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
